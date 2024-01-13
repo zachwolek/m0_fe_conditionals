fdd9dd3dab8359c3ea2257e7495885a8b90b87f7
Zach Wolek <zach.wolek@gmail.com>
2024-01-13 00:52:04 -0600

    Be at the last answer

--- comparisons.js
+++ comparisons.js
@@ -18,13 +18,13 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
 // this should log: false
-
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberStudents === stringTeachers);
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
-
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
-
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 
 // #-------------------
 // PART 2: Articulating what you are doing
@@ -36,12 +36,12 @@ var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
-
+// The Line of Code is seeing if the number of friends is greater than the number of siblings
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+// The Line of Code is seeing if the number of attendees is not strictly equal to the number of meals (that are likely being prepped for an event)
 
 // #-------------------
 // PART 3: Logical Operators
@@ -63,10 +63,14 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log("Does the dog love to play AND love treats?", lovesToPlay && lovesTreats);
 // YOU DO:
 // Determine if the dog loves to play or loves the dog park
-
+console.log("Does The dog love to play OR love the dog park?", lovesToPlay || lovesDogPark);
 // YOU DO:
 // Determine if the dog loves to play and is a puppy
 // HINT: Use the age variable and assume that a puppy is less than 2 years old
+//If (age < 2){
+
+
+console.log("Does the dog love to play AND is a puppy?", lovesToPlay && puppy);
